ae6f606c06be58160ae415c0d7b9dc2ad251a6a2
hyun6ik <fkanwm@naver.com>
2022-04-14 18:08:47 +0900

    Add #10 : AppConfig - 인터페이스 의존성 추가

--- src/main/java/baseball/config/AppConfig.java
+++ src/main/java/baseball/config/AppConfig.java
@@ -2,11 +2,17 @@ package baseball.config;
 
 import baseball.application.game.GameFacade;
 import baseball.domain.ball.BallService;
+import baseball.domain.game.GameMessenger;
+import baseball.domain.game.GameProcessor;
 import baseball.domain.game.GameReferee;
 import baseball.domain.game.GameService;
+import baseball.domain.game.validator.GameValidator;
 import baseball.infrastructure.ball.BallServiceImpl;
+import baseball.infrastructure.game.GameMessengerImpl;
+import baseball.infrastructure.game.GameProcessorImpl;
 import baseball.infrastructure.game.GameRefereeImpl;
 import baseball.infrastructure.game.GameServiceImpl;
+import baseball.infrastructure.game.validator.GameValidatorImpl;
 import baseball.interfaces.game.GameController;
 
 
@@ -21,16 +27,30 @@ public class AppConfig {
     }
 
     public GameService gameService() {
-        return new GameServiceImpl(gameQuestioner());
+        return new GameServiceImpl(gameReferee(), gameMessenger(), gameValidator());
     }
 
     public BallService ballService() {
         return new BallServiceImpl();
     }
 
-    public GameReferee gameQuestioner() {
+    public GameReferee gameReferee() {
         return new GameRefereeImpl();
     }
+
+    public GameMessenger gameMessenger() {
+        return new GameMessengerImpl();
+    }
+
+    public GameProcessor gameProcessor() {
+        return new GameProcessorImpl();
+    }
+
+    public GameValidator gameValidator() {
+        return new GameValidatorImpl();
+    }
+
+
 
 
 }
